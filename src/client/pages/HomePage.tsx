b41b3558daf58b3c324e7bc7f9351ab977da94f1
Jeremy C <102775355+JeremyC3@users.noreply.github.com>
2023-12-23 19:20:35 -0800

    wrapped homepage in centering wrapper

--- src/client/pages/HomePage.tsx
+++ src/client/pages/HomePage.tsx
@@ -1,12 +1,15 @@
 import Grid from "../components/Grid";
 import json from "../../server/demoData.json";
+import MaxWidthWrapper from "../components/MaxWidthWrapper";
 
 function HomePage() {
 	console.log(json.data);
 	return (
 		<div className="homePage">
 			<h1>Home Page</h1>
-			<Grid answers={json.data} />
+			<MaxWidthWrapper>
+				<Grid answers={json.data} />
+			</MaxWidthWrapper>
 			<div>Points:1000</div>
 		</div>
 	);
