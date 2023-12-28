e4372ca66db0bde99a40bbb5e38ef62504b3b763
Samuel Lee <leesamuel423@gmail.com>
2023-12-27 22:57:55 -0500

    Setup Testing

---
+++ jest.config.cjs
@@ -0,0 +1,10 @@
+module.exports = {
+	testEnvironment: "jest-environment-jsdom",
+	transform: {
+		"^.+\\.[t|j]sx?$": "babel-jest",
+	},
+	moduleNameMapper: {
+		"^@/(.*)$": "<rootDir>/src/$1",
+	},
+	setupFilesAfterEnv: ["<rootDir>/src/jest-setup.ts"], // if you have setup file
+};
