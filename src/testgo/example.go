8710201fc1cdfa5d84cc44c66bc483e314d707de
Jeremy C <102775355+JeremyC3@users.noreply.github.com>
2024-01-12 20:04:37 -0800

    testing if the issue is just in the outputs

--- src/testgo/example.go
+++ src/testgo/example.go
@@ -13,5 +13,8 @@ func main() {
 	fmt.Println("Hello, 世界")
 	fmt.Printf("%s exists: %t\n", temp_URL, missing)
 	fmt.Printf("%s A1 env exists: %t\n", a, b)
+	for i := range strings.Split(a, ""){
+		fmt.Printf("I acknolwedge there is a value here %d\n", i)
+	}
 	fmt.Println(newStr)
 }
