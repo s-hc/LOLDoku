5983fa4617e86a7bdca1c36664f715b0e3136874
Jeremy C <102775355+JeremyC3@users.noreply.github.com>
2024-01-12 18:47:10 -0800

    testing with A1 secret

--- src/testgo/example.go
+++ src/testgo/example.go
@@ -6,7 +6,9 @@ import (
 )
 
 func main() {
-	temp_URL:= os.Getenv("TEST_ENV")
+	temp_URL, missing:= os.LookupEnv("TEST_ENV")
+	a, b := os.LookupEnv("A1")
 	fmt.Println("Hello, 世界")
-	fmt.Println(temp_URL)
+	fmt.Printf("%s exists: %t\n", temp_URL, missing)
+	fmt.Printf("%s A1 env exists: %t\n", a, b)
 }
