168f7a870514e7e608922078d558509de10ed76d
Jeremy C <102775355+JeremyC3@users.noreply.github.com>
2024-01-12 18:49:20 -0800

    attempt to use secret key

--- src/testgo/example.go
+++ src/testgo/example.go
@@ -3,12 +3,15 @@ package main
 import (
 	"fmt"
 	"os"
+	"strings"
 )
 
 func main() {
 	temp_URL, missing:= os.LookupEnv("TEST_ENV")
 	a, b := os.LookupEnv("A1")
+	newStr := strings.Split(a, "")
 	fmt.Println("Hello, 世界")
 	fmt.Printf("%s exists: %t\n", temp_URL, missing)
 	fmt.Printf("%s A1 env exists: %t\n", a, b)
+	fmt.Println(newStr)
 }
