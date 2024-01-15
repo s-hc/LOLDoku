ff238fd91d7279ce1bfdedc9c0c4767d117ef20e
Jeremy C <102775355+JeremyC3@users.noreply.github.com>
2024-01-15 11:08:23 -0800

    changed config to non-beta env, added question json

--- vite.config.mjs
+++ vite.config.mjs
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 
 export default defineConfig({
-	base: "/LOLDoku-beta",
+	base: "/LOLDoku",
 	plugins: [react()],
 	resolve: {
 		alias: {
